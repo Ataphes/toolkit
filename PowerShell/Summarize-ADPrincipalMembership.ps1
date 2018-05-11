85b56b059ab54ab135af968d1a2c4df35fda7e54
Ataphes <joseph.ryan.walczak@gmail.com>
2018-05-11 12:57:39 -0400

    Finished up commenting and cleanup.

--- PowerShell/Summarize-ADPrincipalMembership.ps1
+++ PowerShell/Summarize-ADPrincipalMembership.ps1
@@ -1,6 +1,6 @@
 ## Summarize-ADPrincipalMembership
 ## USAGE ##
-## Query an OU for groups and if their occurance is greater than the threshold of the count variable, output the name of the group
+## Helps determine the baseline group membership for new users based on OU.
 
 ## FUTURE CHANGES ##
 ## Enforce POSH Best Practice guidelines (https://github.com/PoshCode/PowerShellPracticeAndStyle)
@@ -17,12 +17,14 @@ $membershipRatio = .8
 ## Prompts user to input credentials for use later in the script. Need to find out if creds are stored in plain text or not.
 $C = Get-Credential
 
-## Counts how many users are in the selected OU and applys the tunable ratio
+## Counts how many users are in the selected OU, Applys the tunable ratio, 
+## and rounds to the nearest whole number
 $ouUsers = Get-ADUser -Filter * -SearchBase $ouInput -Credential $C
 $ouUserCount = $ouUsers.SamAccountName | Measure-Object -Line
 $membershipAdjustedRaw = $ouUserCount.Lines * $membershipRatio
 $membershipAdjustedRounded = [math]::Round($membershipAdjustedRaw)
 
-## Begin Logic
+## Fetches the users in the specified OU, Gets a list of their group memberships, Groups the objects, 
+## Counts the group occurance to see if it's higher than the Membership ratios minimum,
+## and displays the ones that meet the criteria
 $ouUsers.SamAccountName | ForEach-Object { get-adprincipalgroupmembership $_ } | group-object -property name | Where-Object {$_.Count -ge $membershipAdjustedRounded} | Select-Object name | sort-object name
-## End Logic
