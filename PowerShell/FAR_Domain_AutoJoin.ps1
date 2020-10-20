3973945f221d976a33ca31729b909f183b5702d8
Joseph <38984539+Ataphes@users.noreply.github.com>
2020-10-20 11:02:46 -0400

    Fixed domain name field

--- PowerShell/FAR_Domain_AutoJoin.ps1
+++ PowerShell/FAR_Domain_AutoJoin.ps1
@@ -4,4 +4,4 @@ $AdminAccount_PasswordText = Get-Content 'D:\AltirisCLI.txt'
 $AdminAccount_PasswordEncrypted = $AdminAccount_PasswordText | ConvertTo-SecureString
 $PSCredObj = New-Object System.Management.Automation.PSCredential -ArgumentList $AdminAccount_Name, $AdminAccount_PasswordEncrypted
 
-Add-Computer -DomainName 'os.oaklandschools.net' -Credential $PSCredObj -OUPath 'OU=FPSComputers,OU=Farmington,DC=os,DC=oaklandschools,DC=net'
+Add-Computer -DomainName 'OSLAN' -server 'OSDC01.os.oaklandschools.net' -Credential $PSCredObj -OUPath 'OU=FPSComputers,OU=Farmington,DC=os,DC=oaklandschools,DC=net'
