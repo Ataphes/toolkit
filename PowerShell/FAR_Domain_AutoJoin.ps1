4f7eaff5d69453bc064a8557d5a77d7cb3e84f13
Joseph <38984539+Ataphes@users.noreply.github.com>
2020-10-20 09:14:47 -0400

    Testing - Domain Join command

--- PowerShell/FAR_Domain_AutoJoin.ps1
+++ PowerShell/FAR_Domain_AutoJoin.ps1
@@ -3,3 +3,5 @@ $AdminAccount_Name = 'OSLAN\AltirisCLI'
 $AdminAccount_PasswordText = Get-Content 'D:\AltirisCLI.txt'
 $AdminAccount_PasswordEncrypted = $AdminAccount_PasswordText | ConvertTo-SecureString
 $PSCredObj = New-Object System.Management.Automation.PSCredential -ArgumentList $AdminAccount_PasswordText, $AdminAccount_PasswordEncrypted
+
+Add-Computer -DomainName 'os.oaklandschools.net' -Credential $PSCredObj -OUPath 'OU=FPSComputers,OU=Farmington,DC=os,DC=oaklandschools,DC=net'
