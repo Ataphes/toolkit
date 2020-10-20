3e1fededf1fad7c7832c94a069ae9f0f4f932441
Joseph <38984539+Ataphes@users.noreply.github.com>
2020-10-20 09:27:10 -0400

    Messed up calling variables, line 5 fixed

--- PowerShell/FAR_Domain_AutoJoin.ps1
+++ PowerShell/FAR_Domain_AutoJoin.ps1
@@ -2,6 +2,6 @@
 $AdminAccount_Name = 'OSLAN\AltirisCLI'
 $AdminAccount_PasswordText = Get-Content 'D:\AltirisCLI.txt'
 $AdminAccount_PasswordEncrypted = $AdminAccount_PasswordText | ConvertTo-SecureString
-$PSCredObj = New-Object System.Management.Automation.PSCredential -ArgumentList $AdminAccount_PasswordText, $AdminAccount_PasswordEncrypted
+$PSCredObj = New-Object System.Management.Automation.PSCredential -ArgumentList $AdminAccount_Name, $AdminAccount_PasswordEncrypted
 
 Add-Computer -DomainName 'os.oaklandschools.net' -Credential $PSCredObj -OUPath 'OU=FPSComputers,OU=Farmington,DC=os,DC=oaklandschools,DC=net'
