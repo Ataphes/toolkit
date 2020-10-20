dad0777a3030c1fa891f1ca92c3597f98be3549c
Joseph <38984539+Ataphes@users.noreply.github.com>
2020-10-20 15:03:59 -0400

    Created Domain Membership check.

--- PowerShell/FAR_Domain_AutoJoin.ps1
+++ PowerShell/FAR_Domain_AutoJoin.ps1
@@ -1,9 +1,13 @@
-## Check if machine is already joined to domain, if so, do nothing. Or maybe add some groups
+## Test if device able to ping DC to verify on network.
 
-
+## Check if machine is already joined to domain.
+if ((Get-WMI -Class Win32_ComputerSystem).Domain -eq 'oaklandschools.net') {
+write-host 'Machine joined to OSLAN already, exiting'
+exit 0
+}
+Write-Host 'Domain Join Required'
 
 ## Check for AltirisCLI.txt on a USB drive by testing the path. Set VAR if the path comes back true.
-
 $AdminAccount_PublicKey = Get-psdrive -PSProvider 'FileSystem' | ForEach-Object -Process {Join-Path -Path $_.Root -ChildPath 'AltirisCLI.txt' -Resolve -ErrorAction SilentlyContinue}
 
 ## Load Secure String and Create PSCred Object from file on USB drive to ensure security in the domain join process.
@@ -12,7 +16,7 @@ $AdminAccount_PasswordText = Get-Content -path $AdminAccount_PublicKey
 $AdminAccount_PasswordEncrypted = $AdminAccount_PasswordText | ConvertTo-SecureString
 $PSCredObj = New-Object System.Management.Automation.PSCredential -ArgumentList $AdminAccount_Name, $AdminAccount_PasswordEncrypted
 
-## Test if device able to ping DC to verify on network.
+
 
 ## Join Domain without restarting to allow for the addition of groups
 
