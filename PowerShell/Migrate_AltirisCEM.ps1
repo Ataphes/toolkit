553229c489be50537106fb72b0688677ef1c047b
Joseph <38984539+Ataphes@users.noreply.github.com>
2020-10-30 15:17:28 -0400

    Added todo tasks

--- PowerShell/Migrate_AltirisCEM.ps1
+++ PowerShell/Migrate_AltirisCEM.ps1
@@ -2,5 +2,8 @@
 ## Migrate current Altiris endpoints to Altiris Cloud Endpoint Management ##
 ## Designed around the intent to be run on startup via Task Scheduler to allow for resiliance through reboots ##
 
-## TODO: Literally everything (lol)
-## TODO: Dete
+## TODO: Literally everything (lol).
+## TODO: Detect current installation status (Directory check and/or check services).
+## TODO: Install CEM.
+## TODO: Reinstall old client on failure to complete.
+## TODO: Log all output for remediation.
