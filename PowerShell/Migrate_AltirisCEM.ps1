5b23106524df145ad829557b809ad02f2977b2b2
Joseph <38984539+Ataphes@users.noreply.github.com>
2020-10-30 14:55:05 -0400

    Added Migrate_AltirisCEM script.

---
+++ PowerShell/Migrate_AltirisCEM.ps1
@@ -0,0 +1,6 @@
+## Author: Joseph Walczak (Joseph.Walczak@oakland.k12.mi.us) ##
+## Migrate current Altiris endpoints to Altiris Cloud Endpoint Management ##
+## Designed around the intent to be run on startup via Task Scheduler to allow for resiliance through reboots ##
+
+## TODO: Literally everything (lol)
+## TODO: Dete
