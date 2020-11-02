b3545d6162081dd0c9a3c7037df79ffeff275a6a
Walczak <Joseph.Walczak@oakland.k12.mi.us>
2020-11-02 14:27:56 -0500

    Preflight checks begunx

--- PowerShell/Migrate_AltirisCEM.ps1
+++ PowerShell/Migrate_AltirisCEM.ps1
@@ -1,8 +1,10 @@
-## Author: Joseph Walczak (Joseph.Walczak@oakland.k12.mi.us) ##
-## Migrate current Altiris endpoints to Altiris Cloud Endpoint Management ##
-## Designed around the intent to be run on startup via Task Scheduler to allow for resiliance through reboots ##
+## Author: Joseph Walczak (Joseph.Walczak@oakland.k12.mi.us) 
+## Migrate current Altiris endpoints to Altiris Cloud Endpoint Management 
+## Designed around the intent to be run on startup via Task Scheduler to allow for resiliance through reboots
+## Strictly proof of concept, requires to be logged in as local administrator account from a USB drive
+## Can be easily adapted later for production use.
 
-## TODO: Literally everything (lol).
+## TODO: Literally everything.
 ## TODO: Download required files from share if not found. Possibly via google drive
 ## TODO: Detect current installation status (Directory check and/or check services).
 ## TODO: Install CEM.
@@ -10,24 +12,22 @@
 ## TODO: Log all output for remediation incase of failure.
 ## TODO: Set self in task scheduler to ensure completion and remove on success.
 
-## VARIABLES
+Clear-Host
+
+
+
+## Check for staging directory and copy the required files over from USB drive
 $StagingDir = "C:\PostInstall\scripts\Migrate_AltirisCEM"
-$LogDir = "C:\PostInstall\scripts\Migrate_AltirisCEM\logs"
-
-
-## SCRIPT START ##
-
-## check for directory and copy files to staging location
-if ({test-path -path $StagingDir} -false) {
-    
+$Check_StagingDir = Test-Path -LiteralPath $StagingDir
+if ($Check_StagingDir -eq $False) {
+    New-Item -Path $StagingDir -ItemType Directory
+    Copy-Item -Path .* -Destination $StagingDir
 }
 
-## Check if Altiris Client is currently installed and configured correctly
+## Schedule Task for execution.
 
-
-## Uninstall old altiris client and restart
-## SUBTASK: Create scheduled task for resiliance
-
-
-
-## Install CEM
+$TaskName = "Migrate_AltirisCEM"
+$Check_TaskName = Get-ScheduledTask -TaskName $TaskName -ErrorAction SilentlyContinue
+if ($Check_TaskName.state -eq '') {
+    Write-Host 'You Done Goofed Correctly'
+}
