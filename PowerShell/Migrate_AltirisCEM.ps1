08346d6ff6ea4045c11c7f950e48540ef255e627
Joseph <38984539+Ataphes@users.noreply.github.com>
2020-11-03 13:45:54 -0500

    Initial checks complete.

--- PowerShell/Migrate_AltirisCEM.ps1
+++ PowerShell/Migrate_AltirisCEM.ps1
@@ -4,7 +4,6 @@
 ## Strictly proof of concept, requires to be logged in as local administrator account from a USB drive
 ## Can be easily adapted later for production use.
 
-## TODO: Literally everything.
 ## TODO: Download required files from share if not found. Possibly via google drive
 ## TODO: Detect current installation status (Directory check and/or check services).
 ## TODO: Install CEM.
@@ -12,22 +11,29 @@
 ## TODO: Log all output for remediation incase of failure.
 ## TODO: Set self in task scheduler to ensure completion and remove on success.
 
-Clear-Host
-
-
+## SCRIPT START
 
 ## Check for staging directory and copy the required files over from USB drive
+
 $StagingDir = "C:\PostInstall\scripts\Migrate_AltirisCEM"
 $Check_StagingDir = Test-Path -LiteralPath $StagingDir
 if ($Check_StagingDir -eq $False) {
     New-Item -Path $StagingDir -ItemType Directory
     Copy-Item -Path .* -Destination $StagingDir
 }
+Write-Host 'All directories created and files copied.'
 
-## Schedule Task for execution.
+## Schedule Task for execution to establish persistence.
 
 $TaskName = "Migrate_AltirisCEM"
-$Check_TaskName = Get-ScheduledTask -TaskName $TaskName -ErrorAction SilentlyContinue
-if ($Check_TaskName.state -eq '') {
-    Write-Host 'You Done Goofed Correctly'
+$Check_TaskName = Get-ScheduledTask -TaskName $TaskName
+if ($Check_TaskName.TaskName -notcontains $TaskName) {
+    Write-Host 'No Task Detected, Scheduling Task...'
+    schtasks.exe /Create /XML "$StagingDir\Migrate_AltirisCEM.xml"
 }
+Write-Host 'Scheduled task already present.'
+
+
+## Check for and uninstall baseline Altiris Client.
+
+
