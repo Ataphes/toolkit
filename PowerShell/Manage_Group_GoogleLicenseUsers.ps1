d35df2729e44f5d0514635d68a7c7c9f9912f010
Joseph <joseph.ryan.walczak@gmail.com>
2021-10-13 14:49:36 -0400

    First Proof of Concept

--- PowerShell/Manage_Group_GoogleLicenseUsers.ps1
+++ PowerShell/Manage_Group_GoogleLicenseUsers.ps1
@@ -1,3 +1,15 @@
 ## Author: Joseph Walczak (Joseph.Walczak@oakland.k12.mi.us)
-## Purpose: Maintain a list of group members from an aggregate of AD groups and add them to a google admin group.
+## Purpose: Maintain a list of group members from an aggregate of AD groups and add them to an active directory group.
 
+# Variables
+$GroupList = @('G_FPS_Google_ALLAdministrators', 'G_FPS_Google_ALLESP', 'G_FPS_Google_ALLFASA', 'G_FPS_Google_ALLFEA', 'G_FPS_Google_AllNonUnit', 'G_FPS_Google_ALLSecretaries')
+$TargetGroup = 'G_FPS_Google_Education_License'
+
+# Get unsorted user list from GroupList
+$UList = foreach ($Group in $GroupList) {
+    (Get-ADGroupMember -Identity $Group).SamAccountName
+}
+
+# Sort, Deduplicate, and add users to target group.
+$UList_Sorted = $UList | Sort-Object | Get-Unique
+Add-ADGroupMember -Identity $TargetGroup -Members $UList_Sorted
