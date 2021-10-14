1acc0f50f2390172b1697ef7fc3d5a1792d3ff86
Joseph <joseph.ryan.walczak@gmail.com>
2021-10-14 12:52:44 -0400

    Cleanup on the bitlocker script.

--- PowerShell/Rollout_BDE.ps1
+++ PowerShell/Rollout_BDE.ps1
@@ -1,22 +1,29 @@
 ## Author: Joseph Walczak (Joseph.Walczak@oakland.k12.mi.us)
 ## Summary: Check if device is able to communicate with AD before BitLocker
 
+# Variables
+
+$NetCheck = (Test-NetConnection -ComputerName "OSDC02.OS.Oaklandschools.net").PingSucceeded
+
 # Set execution policy for script.
 if ((Get-ExecutionPolicy) -ne 'RemoteSigned') {
     Write-Host "Execution Policy Misconfigured, Correcting..."
     Set-ExecutionPolicy RemoteSigned -Force
 }
-Write-Host "Execution Policy Set to Remote Signed..."
+Write-Host "Execution Policy Set to Remote Signed, no action required."
 
-$NetCheck = Test-NetConnection -computername osdc02.os.oaklandschools.net
-
-# Test connectivity to DC
-if (($NetCheck).PingSucceeded -ne 'True') {
-    throw "Device not connected to OSLAN, Exiting"
+# Test connectivity to DC and stop script if 
+if ($NetCheck -eq $false) {
+    Write-Host "Not connected to OSLAN, Exiting"
+    exit 1
 }
 
-if (($NetCheck).PingSucceeded -eq 'True') {
-    Write-Host 'Device Connected to OSLAN, Proceeding with Bitlocker Deployment.'
-    gpupdate /force
-    Enable-Bitlocker -MountPoint "C:" -EncryptionMethod AES256 -UsedSpaceOnly –RecoveryPasswordProtector -WhatIf
+if ($NetCheck -eq $true) {
+    Write-Host "Device Connected to OSLAN, Updating Group Policy..."
+    Invoke-GPUpdate -Force
+    Write-Host "Done."
+    Write-Host "Enabling Bitlocker Disk Encryption..."
+    Enable-Bitlocker -MountPoint C: -EncryptionMethod AES256 -UsedSpaceOnly -RecoveryPasswordProtector
+    Write-Host "Enabled."
+    exit 0
 }
