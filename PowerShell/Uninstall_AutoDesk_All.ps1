1391530a32434e6587784e94416f980b1e1137ef
Joseph <joseph.ryan.walczak@gmail.com>
2021-11-29 13:56:14 -0500

    Vanity comments and var cleanup.

--- PowerShell/Uninstall_AutoDesk_All.ps1
+++ PowerShell/Uninstall_AutoDesk_All.ps1
@@ -1,7 +1,8 @@
 ## AutoDesk Removal Script
-## Get all AutoDesk products on machine and add to object
-## Author: Joseph Walczak
+## Remove ALL AutoDesk Products from a machine with EXTREME gusto.
+## Author: Joseph Walczak (Joseph.Walczak@oakland.k12.mi.us)
 
+## Safety Off, Full Send.
 $ErrorActionPreference = "SilentlyContinue"
 
 ## Stop all AutoDesk related Processes
@@ -9,20 +10,20 @@ $AD_Proc = Get-Process | Where {$_.Product -Like "*AutoDesk*"}
 Stop-Process -InputObject $AD_Proc -Force
 
 ## Stop all AutoDesk related services.
-$AutoDesk_Service = Get-Service | Where {$_.DisplayName -like "*AutoDesk*"}
-Stop-Service -InputObject $AutoDesk_Service -Force -NoWait
+$AD_Svc = Get-Service | Where {$_.DisplayName -like "*AutoDesk*"}
+Stop-Service -InputObject $AD_Svc -Force -NoWait
 
 ## Autodesk Provided command to remove Autodesk App Manager
 Remove-item "C:\Programdata\Autodesk\SDS" -Recurse -Force
 Start-process -FilePath "C:\Program Files (x86)\Autodesk\Autodesk Desktop App\removeAdAppMgr.exe" -ArgumentList "--mode unattended" -NoNewWindow -Wait 
 
 ## Find all AutoDesk based MSI install files on the machine and put their product IDs in to an array.
-$Reg = @( "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*", "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" )
-$InstalledApps = Get-ItemProperty $Reg -EA 0
-$WantedApp = $InstalledApps | Where { ($_.Publisher -like "*Autodesk*") -and ($_.PSChildName) -match "{" }
-$MSI_ProdID = Out-String -InputObject $WantedApp.PSChildName -Stream -Width 38
+$RegApps = @( "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*", "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" )
+$AllApps = Get-ItemProperty $RegApps -EA 0
+$AD_Apps = $AllApps | Where { ($_.Publisher -like "*Autodesk*") -and ($_.PSChildName) -match "{" }
+$Arr_AD_ProdID = Out-String -InputObject $AD_Apps.PSChildName -Stream -Width 38
 
-foreach ($i in $MSI_ProdID) {
+foreach ($i in $Arr_AD_ProdID) {
 Write-Host Attempting to uninstall Product ID: $i ...
 Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList "/x $i /qn /LI+ C:\AutoDesk_Uninstall.log" -NoNewWindow -Wait
 }
