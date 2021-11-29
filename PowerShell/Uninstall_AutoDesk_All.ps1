8cd094e9b6ae27effae53965738de019d9bd2286
Joseph <joseph.ryan.walczak@gmail.com>
2021-11-29 11:43:44 -0500

    Working, improving with service halt.

--- PowerShell/Uninstall_AutoDesk_All.ps1
+++ PowerShell/Uninstall_AutoDesk_All.ps1
@@ -1,17 +1,24 @@
 ## AutoDesk Removal Script
 ## Get all AutoDesk products on machine and add to object
 ## Author: Joseph Walczak
-    
+
+## Find all AutoDesk based MSI install files on the machine and put their product IDs in to an array.
 $Reg = @( "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*", "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" )
 $InstalledApps = Get-ItemProperty $Reg -EA 0
 $WantedApp = $InstalledApps | Where { ($_.Publisher -like "*Autodesk*") -and ($_.PSChildName) -match "{" }
 $MSI_ProdID = Out-String -InputObject $WantedApp.PSChildName -Stream -Width 38
 
+## Stop all AutoDesk related services.
+## $AutoDesk_Service = Get-Service -Name "*AutoDesk*"
+
+
+
 foreach ($i in $MSI_ProdID) {
-    do {
-        $Service_MSIServer = Get-Service msiserver
-        start-sleep 2
-    } until ($Service_MSIServer.Status -eq "Stopped")
+##    do {
+##       $Service_MSIServer = Get-Service msiserver
+##        start-sleep 15
+##    } until ($Service_MSIServer.Status -eq "Stopped")
 Write-Host Attempting to uninstall Product ID: $i ...
-msiexec.exe /x $i /qb
+Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList "/x $i /qn /LI+ C:\AutoDesk_Uninstall.log" -NoNewWindow -Wait
 }
+## Restart-Computer -Wait 0
