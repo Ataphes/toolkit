c424e8019e03a3c8210667447eab8467bc935db6
Joseph <joseph.ryan.walczak@gmail.com>
2021-12-02 11:08:57 -0500

    Fixed some minor things and fancied up the output.

--- PowerShell/Stall_MSIExec.ps1
+++ PowerShell/Stall_MSIExec.ps1
@@ -4,8 +4,13 @@
 
 # Set duration of run in seconds.
 $Duration_Seconds = 600
+
 do {
-    Stop-Process -Name "msiexec" -ErrorAction SilentlyContinue
+    $TargetProc = Get-Process | where { $_.Name -like "*MSIExec*" }
+    if ($TargetProc -ne $Null) {
+        Stop-Process -InputObject $TargetProc -Force
+        Write-Host "Stopped MSIExec at $(Get-Date -Format "MM/dd/yy HH:MM:ss")"
+    }
     $Duration_Seconds--
     Start-Sleep -Seconds 1
 } until ($Duration_Seconds -eq 0)
