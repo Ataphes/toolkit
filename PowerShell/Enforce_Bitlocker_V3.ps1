97e80164f8cc6fb77aa12f40bf7855b60ac5ef73
Ataphes <joseph.ryan.walczak@gmail.com>
2023-10-03 11:32:29 -0400

    Finally fucking finishing the bitlocker script.

--- PowerShell/Enforce_Bitlocker_V3.ps1
+++ PowerShell/Enforce_Bitlocker_V3.ps1
@@ -4,7 +4,7 @@
 # Functions
 
 # Allow for timestamps for each phase of log.
-function Time {
+function Get-Time {
     return Get-Date
 }
 # Check key status.
@@ -14,55 +14,61 @@ function Get-LocalKey {
 function Get-RemoteKey {
     Get-ADObject -Filter 'ObjectClass -eq "msFVE-RecoveryInformation"' -SearchBase $AD_CurrentMachine.DistinguishedName -Properties whenCreated, msFVE-RecoveryPassword | Sort-Object whenCreated -Descending | Select-Object -First 1 -ExpandProperty msFVE-RecoveryPassword
 }
-function Display_BitLockerRecoveryKeys {
+# Used for debugging and verification of output. Can be removed if/when script is minified.
+function Show-CurrentKeys {
     Write-Output "
 Local Recovery Key: $(Get-LocalKey)
-Remote Recovery Key: $(Get-Remote)
+Remote Recovery Key: $(Get-RemoteKey)
 "
 }
 
 # START SCRIPT
 
-Write-Output "$(Time) | Checking Module Dependencies..."
+Write-Output "$(Get-Time) | Checking Module Dependencies..."
 $InstalledFeatures = Get-WindowsCapability -Name *Rsat.ActiveDirectory* -Online
 
 if ($InstalledFeatures.State -ne "Installed") {
-    Write-output "$(Time) | Active Directory Feature not installed, Installing Feature..."
+    Write-output "$(Get-Time) | Active Directory Feature not installed, Installing Feature..."
     $InstalledFeatures | Add-WindowsCapability -Online > $null
-    Write-Output "$(Time) | Installed Active Directory Feature. Importing Active Directory Module..."
+    Write-Output "$(Get-Time) | Installed Active Directory Feature. Importing Active Directory Module..."
     Import-Module -Name ActiveDirectory
 }
 else {
-    Write-Output "$(Time) | Active Directory Feature is Installed, Importing Active Directory Module..."
+    Write-Output "$(Get-Time) | Active Directory Feature is Installed, Importing Active Directory Module..."
     Import-Module -Name ActiveDirectory
 }
 
-Write-Output "$(Time) | Verifying connectivity to DC..."
+Write-Output "$(Get-Time) | Verifying connectivity to DC..."
 
 $DC = (Get-ADDomainController -NextClosestSite -Discover -DomainName "os.oaklandschools.net" -ErrorAction SilentlyContinue).hostname
 
 if ($null -eq $DC) {
-    Write-Output  "$(Time) | Unable to connect to DC, Exiting..."
+    Write-Output  "$(Get-Time) | Unable to connect to DC, Exiting..."
     Exit 1
 }
 else {
-    Write-Output "$(Time) | Successfully Connected to: $DC"
+    Write-Output "$(Get-Time) | Successfully Connected to: $DC"
 }
 
-Write-Output "$(Time) | Checking Recovery Key Status..."
+Write-Output "$(Get-Time) | Checking Recovery Key Status..."
 
 $SysDrive = $ENV:SYSTEMDRIVE
 $AD_CurrentMachine = Get-ADComputer $ENV:COMPUTERNAME
 
+# Verifies and resolves key pair mismatches.
 if (-not $(Get-RemoteKey -ReturnRecoveryKey) -or -not $(Get-RemoteKey -ReturnRecoveryKey) -or $(Get-LocalKey -ReturnRecoveryKey) -ne $(Get-RemoteKey -ReturnRecoveryKey)) {
-    Write-Output "$(Get-TimeStamp) | BitLocker Recovery Keys are Mismatched or Missing. Attempting to Generate and Sync New Recovery Key..."
-    if (-not $(Get-RemoteKey -ReturnRecoveryKey)) {
-        Write-Output "$(Get-TimeStamp) | Enabling and Bitlocker and generating new Local Recovery Key..."
-        Enable-Bitlocker -MountPoint $SysDrive -EncryptionMethod AES256 -UsedSpaceOnly -RecoveryPasswordProtector
+    Write-Output "$(Get-Time) | BitLocker Recovery Keys are Mismatched or Missing. Attempting to Generate and Sync New Recovery Key..."
+    # Enables Bitlocker Locally, piped to null to hide ugly output.
+    if (-not $(Get-LocalKey -ReturnRecoveryKey)) {
+        Write-Output "$(Get-Time) | Enabling and Bitlocker and generating new Local Recovery Key..."
+        Enable-Bitlocker -MountPoint $SysDrive -EncryptionMethod AES256 -UsedSpaceOnly -RecoveryPasswordProtector -WarningAction SilentlyContinue > $Null
     }
+    # Checks key sync status and sends the updated key to AD if not correct.
     if (-not $(Get-RemoteKey -ReturnRecoveryKey) -or $(Get-LocalKey -ReturnRecoveryKey) -ne $(Get-RemoteKey -ReturnRecoveryKey)) {
-        Write-Output "$(Get-TimeStamp) | Communicating Recovery Key to Active Directory..."
+        Write-Output "$(Get-Time) | Communicating Recovery Key to Active Directory..."
         Backup-BitLockerKeyProtector -MountPoint $SysDrive -KeyProtectorId $(Get-LocalKey -ReturnKeyProtectorID)
     }
 }
-
+# Instrumentation, Can be removed if script is minified.
+Write-Output -InputObject $(Show-CurrentKeys)
+Exit 0
